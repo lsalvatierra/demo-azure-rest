c9054e8cc589c453f7c4f5ef6ee91032db7a8fb2
lsalvatierra <luisangel.salvat@gmail.com>
2026-02-17 21:11:52 -0500

    actualización del response

--- src/main/java/pe/edu/cibertec/demo_azure_rest/controller/HelloController.java
+++ src/main/java/pe/edu/cibertec/demo_azure_rest/controller/HelloController.java
@@ -4,12 +4,18 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 @RestController
 public class HelloController {
 
     //localhos:8080/hello
     @GetMapping("/hello")
-    public ResponseEntity<String> getWelcome(){
-        return ResponseEntity.ok("Hello API Rest, desde AppService-Azure");
+    public ResponseEntity<Map<String, String>> getWelcome(){
+        Map<String, String> response = new LinkedHashMap<>();
+        response.put("app", "Cibertec");
+        response.put("message", "Hello API Rest, desde AppService-Azure");
+        return ResponseEntity.ok(response);
     }
 }
